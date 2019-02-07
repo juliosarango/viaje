f969db952743d8857bda7677abb227e11b1cb6fb
Julio Sarango <jsarangoq@gmail.com>
2019-02-06 23:03:00 -0500

    nuevos componentes

--- App.js
+++ App.js
@@ -1,30 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import AppButton from './application/components/AppButton';
-
+import PreLoader from './application/components/PreLoader';
+import BackgroundImage from './application/components/BackgroundImage';
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hola mundo!!!</Text>
-        <AppButton           
-          action={()=> console.log('click')}
-          iconName="home"
-          iconColor="#fff"
-          title="Home"
-          bgColor="rgba(220,100,20,0.7)"
-        />
 
-      </View>
+  state = {
+    loader: true
+  }
+  componentDidMount() {
+    this.setState({
+      loader: false
+    })
+
+  }
+  render() {
+    return (    
+      <View style={styles.container}>
+        <BackgroundImage 
+         source={require('./assets/images/image-bg.jpg')}
+        />
+        {
+          this.state.loader &&
+          <PreLoader />
+        }
+        
+      </View>      
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1   
   },
 });
